010cbaf69aff3d2fa6170b5fd89b06510104e480
Gaspar Tessi <67377602+gaspartessi@users.noreply.github.com>
2022-05-04 23:47:09 -0300

    Contador ItemCount y ItemListContainer ambos como funciones en vez de componentes

---
+++ src/components/ItemCount.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+
+
+const ItemCount = (props) => {
+
+  const [count, setCount] = useState(parseInt(props.initial));
+
+  const sumarcarrito = () => {
+      
+      if(count < props.stock){
+          setCount(count + 1);
+      }
+  }
+
+  const menorquecero = () => {
+     
+      if(count > 0 ){
+          setCount(count - 1);
+      }
+  }
+
+
+  return(
+      <div>
+          <button onClick={menorquecero}>-</button>
+          <span>{count}</span>
+          <button onClick={sumarcarrito}>+</button>
+      </div>
+  )
+}
+
+export default ItemCount;
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,74 +1,18 @@
 import React, { Component } from 'react';
+import ItemCount from './ItemCount';
 
-const arr = ["Antonio Gaspar Tessi","Jorge Perez", "Juan Gonzalez"];
+/*const arr = ["Antonio Gaspar Tessi","Jorge Perez", "Juan Gonzalez"];
 function Greeting(props) {
     return <h1>Hola Bienvenido a mi tienda {props.name}</h1>;
-}
+}*/
 
-export default class ItemListContainer extends Component {
-    render(){
+const ItemListContainer = () => {
+    
         return(
             <div>
-                <h1><Greeting name={arr[0]}></Greeting></h1>
-            <table>
-                <tr>
-                    <th>NAME</th>
-                    <th>BOTON</th>
-                    <th>Options</th>
-                </tr>
-                
-                <Row></Row>
-                <Row></Row>
-                <Row></Row>
-            </table>
+             <ItemCount stock="22" initial="10" ></ItemCount>   
             </div>
-            )
-    }
+            ); 
 }
 
-class Boton extends Component {
-    render(){
-        return(
-            <button>{this.props.name}</button>
-        )
-    }
-}
-
-class Title extends Component {
-    render(){
-        return(
-            <p>Titulo</p>
-        )
-    }
-}
-
-class Option extends Component {
-    render(){
-        return(
-            <select>
-                <option>1</option>
-                <option>2</option>
-                <option>3</option>
-                <option>4</option>
-            </select>
-        )
-    }
-}
-
-class Row extends Component {
-    render(){
-        return(
-            <tr>
-                <td>
-                    <Title></Title>
-                </td>
-                <td>
-                    <Boton name="ROW"></Boton>
-                </td>
-                <td>
-                    <Option></Option>
-                </td>
-            </tr>
-        )
-    }
-}
+export default ItemListContainer ;
